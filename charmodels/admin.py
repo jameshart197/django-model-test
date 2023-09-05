30bb995e9ab6fa0e3afe4edde74a3cc18d957bc8
james hart <jamesmurphy197197@gmail.com>
2023-09-05 11:55:30 +0000

    DEFINITION LOOP ISSUE

--- charmodels/admin.py
+++ charmodels/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import models
+from django.apps import apps
 
 # Register your models here.
+models = apps.get_models()
+
+for model in models:
+    admin.site.register(model)
